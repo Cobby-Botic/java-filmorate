26b79bea5a42f436c5128e4a8e9d880341670507
User Kovalyov <dan.cowaleff@yandex.ru>
2026-06-15 15:31:12 +0300

    Переписана логика с использованием аннотаций

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,7 +1,11 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDate;
 
@@ -9,8 +13,16 @@ import java.time.LocalDate;
 @Setter
 public class Film {
     private Integer id;
+
+    @NotBlank
     private String name;
+
+    @NotBlank
+    @Length(max = 200)
     private String description;
+
     private LocalDate releaseDate;
+
+    @Min(1)
     private Integer duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,5 +1,8 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +12,16 @@ import java.time.LocalDate;
 @Setter
 public class User {
     private Integer id;
+
+    @Email
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String login;
+
     private String name;
+
+    @PastOrPresent
     private LocalDate birthday;
 }
