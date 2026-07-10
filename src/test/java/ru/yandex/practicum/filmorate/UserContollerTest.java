5f8c0ff9474f78d5951ab037937b2f141f3a42a3
User Kovalyov <dan.cowaleff@yandex.ru>
2026-07-10 10:03:25 +0300

    Checkstyle edits

--- src/test/java/ru/yandex/practicum/filmorate/UserContollerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserContollerTest.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate;
 
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class UserContollerTest {
+class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
